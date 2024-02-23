559a0b582a0127c720487ebbc2f771e57c096466
xDerrick23 <dinodidiodoro23@gmail.com>
2024-02-23 12:24:40 +0100

    Initial commit

---
+++ src/main/java/dinodidiodoro/EPICODE/S7L5/EpicodeS7L5Application.java
@@ -0,0 +1,13 @@
+package dinodidiodoro.EPICODE.S7L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeS7L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeS7L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/dinodidiodoro/EPICODE/S7L5/EpicodeS7L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package dinodidiodoro.EPICODE.S7L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeS7L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
